87e76e9160b45fb0356f48e50897f1731c31a9a4
Adriel Villar Vasquez <adriel_villar@ucne.edu.do>
2020-03-25 19:04:30 -0400

    Agregando Tipo Cliente

---
+++ Models/TipoCliente.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalPA2.Models
+{
+    public class TipoCliente
+    {
+
+        [Key]
+
+        public int TipoClienteId { get; set; }
+
+        [Required(ErrorMessage = "El campo Descripcion no puede estar vacio")]
+        [StringLength(maximumLength: 40, ErrorMessage = "Esta fuera de rango")]
+        public string Descripcion { get; set; }
+
+        public TipoCliente()
+        {
+            TipoClienteId = 0;
+            Descripcion = string.Empty
+        }
+    }
+}
