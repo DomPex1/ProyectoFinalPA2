ad87ca142ac107aa6a93aeb313b2acc195a31b82
Adriel Villar Vasquez <adriel_villar@ucne.edu.do>
2020-04-06 12:24:39 -0400

    Commit

--- Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -73,7 +73,7 @@ namespace ProyectoFinalPA2.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl = returnUrl ?? Url.Content("~/");
+            returnUrl = returnUrl ?? Url.Content("~/index");
 
             if (ModelState.IsValid)
             {
